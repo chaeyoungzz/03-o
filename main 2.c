1d3f3bd1c8d8348a1baf883349151de36e5de0d8
chaeyoung <456codud@naver.com>
2018-10-02 15:46:15 +0900

    0:0
    dd

---
+++ main 2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int a;
+	
+	printf("input the second : ");
+	scanf("%i",&a);
+	
+	printf("the time is %i : %i\n ", a/60 , a%60);
+	
+	
+	return 0;
+}
