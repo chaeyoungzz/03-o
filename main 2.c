0dbd1a1b6a07e0090293413a2654b7077598b55c
chaeyoung <456codud@naver.com>
2018-10-02 16:30:37 +0900

    mean
    ss

--- main 2.c
+++ main 2.c
@@ -4,12 +4,13 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int a;
+	int a, b, c;
 	
-	printf("input the second ");
-	scanf("%i", &a);
+	printf("input 3 integers : ");
+	scanf("%i %i %i", &a , &b , &c);
 	
-	printf("The time is %i : %i : %i\n", a/3600 , a%3600/60 , a%60 );
+	printf("The mean of %i, %i, %i is : %f\n", a, b, c, (double)(a+b+c)/3);
+	
 	
 	return 0;
 }
